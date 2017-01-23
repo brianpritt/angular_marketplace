54b84cfe368c6a8f198d258ff106f9e7dc68f368
Brian Pritt and Jonathan Thom <brian+jonathan@>
2017-01-23 14:03:18 -0800

    Display individual album page

--- src/app/marketplace/marketplace.component.ts
+++ src/app/marketplace/marketplace.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { Album } from '../album.model';
+import { Router } from '@angular/router';
+import { AlbumService } from '../album.service';
 
 @Component({
   selector: 'app-marketplace',
   templateUrl: './marketplace.component.html',
-  styleUrls: ['./marketplace.component.css']
+  styleUrls: ['./marketplace.component.css'],
+  providers: [AlbumService]
 })
 export class MarketplaceComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router, private albumService: AlbumService) {}
 
-  ngOnInit() {
+  albums: Album[];
+
+  ngOnInit(){
+    this.albums = this.albumService.getAlbums();
   }
 
+ goToDetailPage(clickedAlbum: Album) {
+   this.router.navigate(['albums', clickedAlbum.id]);
+ };
 }
